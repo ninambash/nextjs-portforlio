10619af8ced0b03201c65b56382ecaf7f43b45b3
ninambash <ninambash@gmail.com>
2023-02-22 10:08:08 -0800

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import '@/styles/globals.css'
-import Navbar from '@/components/Navbar'
+import '@/styles/globals.css';
+import Navbar from '@/components/Navbar';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
+// import { Section } from '../styles/GlobalComponents';
+
+
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -14,13 +18,27 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <main className={styles.main}>
         <div className={styles.description}>
           <h1>Welcome to my portforlio</h1>
-         
-         
         </div>
+       
       </main>
+      <div>
+         {/* <BgAnimation /> */}
+      {/* </Section> */}
+      <Projects />
+      <Technologies />
+      <Timeline />
+      <Accomplishments />
+   
+      </div>
+      <div>
+     
+      </div>
+      
     </>
+     
   )
 }
